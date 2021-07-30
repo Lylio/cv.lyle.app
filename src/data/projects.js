00957b99c69327da893a59d75056fb600fbe1efa
Lylio <cxz@gmx.com>
2021-07-30 15:17:33 +0100

    various updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,44 @@
 // TODO Add a couple lines about each project
+
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'bugout.lylio.tech',
+    subtitle: 'Bug & issue tracker for development teams',
+    link: 'https://bugout.lylio.tech',
+    image: '/images/projects/bugout.jpg',
+    date: '2021-07-21',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'BugOut is an issue/bug tracker application for development teams to document problems'
+      + ' found within their digital estate and assign team members to fix bugs or build new'
+      + ' features.',
+    icon: '/images/lyle-christine.jpg',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    title: 'LyleChristine.com',
+    subtitle: 'Official site for Glasgow-based alt/inDIE/RRRROCK musician',
+    link: 'https://www.lylechristine.com',
+    image: '/images/projects/lylechristinedotcom.jpg',
+    date: '2021-07-24',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      "I write and record music. It's a pain in the arse",
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
+    title: 'Chatty!',
+    subtitle: 'A straightforward chat room app. DO NOT try to sell and/or buy missile launchers here.',
+    link: 'https://chatty-services.herokuapp.com/',
+    image: '/images/projects/chatty.jpg',
+    date: '2021-07-01',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'Good clean chat.',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
+    title: 'Dento',
+    subtitle: 'Book your checkup with your local friendly dentist!',
+    link: 'https://dento-dentistry.herokuapp.com/',
+    image: '/images/projects/dento.jpg',
+    date: '2021-08-30',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'A dental website built with Django!',
   },
 ];
 
--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,27 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'University of Glasgow',
+    degree: 'MSc, Software Development',
+    link: 'https://www.gla.ac.uk/postgraduate/taught/softwaredevelopment/',
+    year: 2018,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'University of Glasgow',
+    degree: 'MPhil, Sociology',
+    link: 'https://www.gla.ac.uk/postgraduate/research/sociology/',
+    year: 2014,
+  },
+  {
+    school: 'University of Glasgow',
+    degree: 'MRes, Sociology & Research Methods',
+    link: 'https://www.gla.ac.uk/postgraduate/taught/sociologyresearchmethods/',
+    year: 2010,
+  },
+  {
+    school: 'Abertay University',
+    degree: 'BA(Hons), Media, Culture & Society',
+    link: 'https://www.abertay.ac.uk/course-search/undergraduate/sociology/',
+    year: 2009,
   },
 ];
 
