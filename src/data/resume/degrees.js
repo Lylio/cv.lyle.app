9a60831b085089ec8d8a28949f6280868689a313
Lylio <cxz@gmx.com>
2023-03-03 10:14:44 +0000

    feature courses

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -6,16 +6,20 @@ const degrees = [
     degree: 'MSc, Software Development',
     link: 'https://www.gla.ac.uk/postgraduate/taught/softwaredevelopment/',
     curriculum: [
-      'Programming (Java) ∙ Advanced Programming (Java) ∙ Database Theory and Applications (SQL)',
-      'Algorithms and Data Structures ∙ Software Engineering (UML, Design Patterns)',
-      'Internet Technology (HTML, CSS, JavaScript, Python, Django)',
-      'Final project: Android mobile app (Java, Android Studio)',
+      ' Programming (Java) ∙ Advanced Programming (Java) ∙ Database Theory and Applications (SQL)',
+      ' ∙ Algorithms and Data Structures ∙ Software Engineering (UML, Design Patterns)',
+      ' ∙ Internet Technology (HTML, CSS, JavaScript, Python, Django)',
+      ' ∙ Final project: Android mobile app (Java, Android Studio)',
     ],
   },
   {
     school: 'University of Glasgow',
     degree: 'MPhil, Sociology',
     link: 'https://www.gla.ac.uk/postgraduate/research/sociology/',
+    curriculum: [
+      'Processes of Recovery from Problem Gambling: A Qualitative Exploration\n',
+      'https://theses.gla.ac.uk/5470/',
+    ],
   },
   {
     school: 'University of Glasgow',
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,15 +14,16 @@ const Index = () => (
         </div>
       </header>
       <h2 style={{ color: 'rgb(134,19,48)' }}>Current Activity</h2>
-      <p>Hi I&apos;m <b>Lyle</b>, welcome to my online CV. I&apos;m delighted to say I&apos;m
-        currently a <b>Senior Software Engineer</b> with esteemed global enterprise
-        <b><a href="https://www.cgi.com/en"> CGI</a></b>. For further info on my career details,
-        please check out my <b><a href="https://cv.lyle.app/about">ABOUT</a></b>
+      <p>Hi welcome to <b>Lyle Christine&apos;s</b> online CV extravaganza! It has skills!
+        Experience!! Contact info!!! All for the affordable price of... <b>free</b>! I&apos;m
+        delighted to say I&apos;m currently a <b>Senior Software Engineer</b> with esteemed global
+        enterprise<b><a href="https://www.cgi.com/en"> CGI</a></b>. For further info on my career
+        details, please check out my <b><a href="https://cv.lyle.app/about">ABOUT</a></b>
         and <b><a href="https://cv.lyle.app/cv">CV</a></b> pages.
       </p>
-      <p> Finally, for a little fun, don&apos;t forget to have a look at my portfolio
+      <p> Finally, for a little fun 🥳, don&apos;t forget to have a look at my portfolio
         <b><a href="https://cv.lyle.app/projects"> PROJECTS</a></b> page for examples of live apps
-        that I&apos;ve built which include links to my GitHub code repos.
+        that I&apos;ve built. Links to my GitHub code repos included as take-home party bags.
       </p>
     </article>
   </Main>
