84c123e4669faba43ec040066c836dcab882ecf6
Lylio <cxz@gmx.com>
2023-03-11 16:37:52 +0000

    feature : intro text toned down

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,16 +14,15 @@ const Index = () => (
         </div>
       </header>
       <h2 style={{ color: 'rgb(134,19,48)' }}>Current Activity</h2>
-      <p>Hi welcome to <b>Lyle Christine&apos;s</b> online CV extravaganza! It has skills!
-        Experience!! Contact info!!! All for the affordable price of... <b>free</b>! I&apos;m
-        delighted to say I&apos;m currently a <b>Senior Software Engineer</b> with esteemed global
-        enterprise<b><a href="https://www.cgi.com/en"> CGI</a></b>. For further info on my career
-        details, please check out my <b><a href="https://cv.lyle.app/about">ABOUT</a></b>
-        and <b><a href="https://cv.lyle.app/cv">CV</a></b> pages.
+      <p>Hi welcome to my online CV. I&apos;m
+        delighted to say I&apos;ll soon occupy the role of <b>Senior Java Developer</b> with
+        esteemed global enterprise<b><a href="https://www.cgi.com/en"> CGI</a></b>.
+        For further info on my career details, please check out my
+        <b><a href="https://cv.lyle.app/about"> ABOUT</a></b> and
+        <b><a href="https://cv.lyle.app/cv"> CV</a></b> pages.
       </p>
-      <p> Finally, for a little fun 🥳, don&apos;t forget to have a look at my portfolio
-        <b><a href="https://cv.lyle.app/projects"> PROJECTS</a></b> page for examples of live apps
-        that I&apos;ve built. Links to my GitHub code repos included as take-home party bags.
+      <p> Finally, my portfolio <b><a href="https://cv.lyle.app/projects"> PROJECTS </a></b>
+        page has some live demos along with my GitHub repos to give you an idea of my skill level.
       </p>
     </article>
   </Main>
