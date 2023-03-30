699ce51f67a0c502860424ac04a356cbcadd48db
Lylio <cxz@gmx.com>
2023-03-30 14:31:15 +0100

    feat : update index info

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -15,14 +15,15 @@ const Index = () => (
       </header>
       <h2 style={{ color: 'rgb(134,19,48)' }}>Current Activity</h2>
       <p>Hi welcome to my online CV. I&apos;m
-        delighted to say I&apos;ll soon occupy the role of <b>Senior Java Developer</b> with
-        esteemed global enterprise<b><a href="https://www.cgi.com/en"> CGI</a></b>.
+        delighted to say I&apos;ll soon occupy the role of <b>Senior Sofware Engineer (Java) </b>
+        with esteemed global enterprise<b><a href="https://www.cgi.com/en"> CGI</a></b>.
         For further info on my career details, please check out my
         <b><a href="https://cv.lyle.app/about"> ABOUT</a></b> and
         <b><a href="https://cv.lyle.app/cv"> CV</a></b> pages.
       </p>
-      <p> Finally, my portfolio <b><a href="https://cv.lyle.app/projects"> PROJECTS </a></b>
-        page has some live demos along with my GitHub repos to give you an idea of my skill level.
+      <p> Also, my portfolio <b><a href="https://cv.lyle.app/projects"> PROJECTS </a></b>
+        page has some live demos along with my GitHub repos to give you an idea of my coding skill
+        level (*<b>cough</b>* ChatGPT level).
       </p>
     </article>
   </Main>
