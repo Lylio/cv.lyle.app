6632f90d93fae188aa6267f8b4b5029c14818f6e
Lylio <cxz@gmx.com>
2023-10-22 16:37:31 +0100

    feat | multiple

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const positions = [
   {
     company: 'HMCTS',
@@ -5,10 +7,11 @@ const positions = [
     link: 'https://www.gov.uk/government/organisations/hm-courts-and-tribunals-service',
     daterange: 'May 2023 - October 2023',
     points: [
-      'Angular 16 ∙ Node 18 ∙ Express ∙ Azure',
-      'JavaScript/TypeScript ∙ HTML5 ∙ CSS3',
-      'Git/GitHub ∙ Figma ∙ Yarn',
-      'Agile Methodologies ∙ Peer Review ',
+      <b>Angular 16 ∙ Node 18 ∙ Express ∙ Azure<br />
+        JavaScript/TypeScript ∙ HTML5 ∙ CSS3<br />
+        Git/GitHub ∙ Figma ∙ Yarn<br />
+        Agile Methodologies ∙ Peer Review
+      </b>,
     ],
     description: 'I worked as a Front-End Angular Developer on a modernisation project '
       + 'for HM Courts & Tribunals Service. This greenfield project focused on migrating and '
@@ -21,8 +24,9 @@ const positions = [
     link: 'https://www.cgi.com/en',
     daterange: 'April 2023 - Present',
     points: [
-      'Software Development & Engineering · Back-End & Front-End Tooling',
-      'Mentoring · Self-Development · Teamwork · Communication',
+      <b>Software Development & Engineering · Back-End & Front-End Tooling<br />
+        Mentoring · Self-Development · Teamwork · Communication<br />
+      </b>,
     ],
     description: 'CGI is an information technology, consulting and systems integration company with '
       + 'a global presence. I\'m a Senior Software Engineer within CGI\'s Scottish Business Unit. '
@@ -35,8 +39,9 @@ const positions = [
     link: 'https://fsbtech.com/',
     daterange: 'Nov 2022 - Dec 2022',
     points: [
-      'Java (JDK 8/11) ∙ Maven ∙ Linux',
-      'PostgreSQL ∙ MongoDB ∙ GitLab',
+      <b>Java (JDK 8/11) ∙ Maven ∙ Linux<br />
+        PostgreSQL ∙ MongoDB ∙ GitLab<br />
+      </b>,
     ],
     description: 'FSB Tech was a self-proclaimed \'gaming technology\' company who peddled a '
       + 'generic and derivative online betting experience. A week before Christmas 2022, FSB closed '
@@ -51,9 +56,10 @@ const positions = [
     link: 'https://www.olmsystems.com/',
     daterange: 'Aug 2021 - Oct 2022',
     points: [
-      'Java (JDK 8/11) ∙ Quarkus ∙ Spring Boot ∙ GraphQL',
-      'PostgreSQL ∙ Kubernetes ∙ Kafka ∙ Docker ∙ Maven',
-      'Google Cloud Platform ∙ Linux (Ubuntu) ∙ Bitbucket ',
+      <b>Java (JDK 8/11) ∙ Quarkus ∙ Spring Boot ∙ GraphQL<br />
+        PostgreSQL ∙ Kubernetes ∙ Kafka ∙ Docker ∙ Maven<br />
+        Google Cloud Platform ∙ Linux (Ubuntu) ∙ Bitbucket <br />
+      </b>,
     ],
     description: 'As part of the integration team, I developed bespoke data services which sat'
       + ' within OLM\'s health & social care software products.'
@@ -71,10 +77,12 @@ const positions = [
     link: 'https://uk.virginmoney.com/',
     daterange: 'Oct 2018 - Jun 2021',
     points: [
-      'OpenShift ∙ Docker ∙ Linux (RHEL 8) ∙ Bash',
-      'Java (JDK 8) ∙ Spring Boot ∙ Python ∙ Gradle ∙ Maven',
-      'Jenkins ∙ Nexus Repo ∙ MongoDB ∙ SonarQube ∙ GoCD',
-      'GitHub ∙ React ∙ Azure ∙ MacOS',
+      <b>
+        OpenShift ∙ Docker ∙ Linux (RHEL 8) ∙ Bash<br />
+        Java (JDK 8) ∙ Spring Boot ∙ Python ∙ Gradle ∙ Maven<br />
+        Jenkins ∙ Nexus Repo ∙ MongoDB ∙ SonarQube ∙ GoCD<br />
+        GitHub ∙ React ∙ Azure ∙ MacOS<br />
+      </b>,
     ],
     description: 'In this DevOps role I developed, supported and maintained'
       + ' Virgin Money\'s Operational Platform which was a complex containerisation estate'
