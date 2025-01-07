52a7a309de132a7d6d9f4d42132274829fe6512e
Lyle Christine <cxz@gmx.com>
2025-01-07 10:45:36 +0000

    new Python cert

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -32,12 +32,11 @@ const Code = () => (
         <ul>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-H4OF8EKL"><img
-              src="https://blob.sololearn.com/certificates/f1e8c045-3c8f-4a00-877b-bc1bb3aba74c.jpg"
+              src="https://imgur.com/I7KKXoq.jpeg"
               alt="Introduction to Python"
             />
             </a>
-            <span>Introduction to Python | Jan 2025 | <a id="cert"
-                                                  href="https://www.sololearn.com/certificates/CC-H4OF8EKL">Certificate</a></span>
+            <span>Introduction to Python | Jan 2025 | <a id="cert" href="https://www.sololearn.com/certificates/CC-H4OF8EKL">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-YJ1Y8S6C"><img
@@ -45,8 +44,7 @@ const Code = () => (
               alt="Coding for Data"
             />
             </a>
-            <span>Coding for Data | Dec 2024 | <a id="cert"
-                                                  href="https://www.sololearn.com/en/certificates/CC-YJ1Y8S6C">Certificate</a></span>
+            <span>Coding for Data | Dec 2024 | <a id="cert" href="https://www.sololearn.com/en/certificates/CC-YJ1Y8S6C">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-5NVB5KDV"><img
@@ -54,8 +52,7 @@ const Code = () => (
               alt="SQL Intermediate"
             />
             </a>
-            <span>SQL Intermediate | Nov 2024 | <a id="cert"
-                                                   href="https://www.sololearn.com/en/certificates/CC-5NVB5KDV">Certificate</a></span>
+            <span>SQL Intermediate | Nov 2024 | <a id="cert" href="https://www.sololearn.com/en/certificates/CC-5NVB5KDV">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-FJIJVBP3"><img
@@ -63,8 +60,7 @@ const Code = () => (
               alt="Java Intermediate"
             />
             </a>
-            <span>Java Intermediate | Sept 2023 | <a id="cert"
-                                                     href="https://www.sololearn.com/certificates/CC-FJIJVBP3">Certificate</a></span>
+            <span>Java Intermediate | Sept 2023 | <a id="cert" href="https://www.sololearn.com/certificates/CC-FJIJVBP3">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-WXYQA2GW"><img
@@ -72,8 +68,7 @@ const Code = () => (
               alt="JavaScript"
             />
             </a>
-            <span>JavaScript | Aug 2023 | <a id="cert"
-                                             href="https://www.sololearn.com/certificates/CC-WXYQA2GW">Certificate</a></span>
+            <span>JavaScript | Aug 2023 | <a id="cert" href="https://www.sololearn.com/certificates/CC-WXYQA2GW">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CC-DVJRYRU8"><img
@@ -81,8 +76,7 @@ const Code = () => (
               alt="SQL"
             />
             </a>
-            <span>SQL | Aug 2023 | <a id="cert"
-                                      href="https://www.sololearn.com/certificates/CC-DVJRYRU8">Certificate</a></span>
+            <span>SQL | Aug 2023 | <a id="cert" href="https://www.sololearn.com/certificates/CC-DVJRYRU8">Certificate</a></span>
           </li>
           <li>
             <a href="https://www.sololearn.com/certificates/CT-6JS4MSTM"><img
@@ -90,12 +84,11 @@ const Code = () => (
               alt="Java"
             />
             </a>
-            <span>Java | March 2023 | <a id="cert"
-                                         href="https://www.sololearn.com/certificates/CT-6JS4MSTM">Certificate</a></span>
+            <span>Java | March 2023 | <a id="cert" href="https://www.sololearn.com/certificates/CT-6JS4MSTM">Certificate</a></span>
           </li>
         </ul>
       </div>
-      <ContactIcons/>
+      <ContactIcons />
     </article>
   </Main>
 );
