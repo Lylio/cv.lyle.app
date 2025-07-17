1002415c41225c715edbcc914e4467777dc0bff1
Lyle Christine <cxz@gmx.com>
2025-07-17 21:31:40 +0100

    code certificate correction

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -18,12 +18,12 @@ const Code = () => (
           <a href="https://leetcode.com/u/Lylio/"><b> LeetCode</b></a> and others to keep my
           knowledge of familiar languages fresh, and also as a way to up-skill into new technical
           areas. While I&#39;ve worked with these programming languages for many years, I still find
-          satisfaction in keeping my skills <i>shiny</i> and <i>sharp</i>. After all: <br/> <br/>
+          satisfaction in keeping my skills <i>shiny</i> and <i>sharp</i>. After all: <br /> <br />
           <a id="rust" href="https://www.youtube.com/watch?v=i6RZY4Ar3fw">Rust Never
             Sleeps
           </a>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <div id="certlist">Completed certificates are listed below:
           </div>
         </div>
@@ -31,80 +31,136 @@ const Code = () => (
       <div className="sololearn">
         <ul>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-7TYIURTO"><img
-              src="https://i.imgur.com/i7SRfMa.png"
-              alt="Python Intermediate"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-7TYIURTO">
+              <img
+                src="https://i.imgur.com/i7SRfMa.png"
+                alt="Python Intermediate"
+              />
             </a>
-            <span>Python Intermediate | Jan 2025 | <a id="cert"
-                                                      href="https://www.sololearn.com/certificates/CC-7TYIURTO">Certificate</a></span>
+            <span>Python Intermediate | Jan 2025 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CC-7TYIURTO"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-H4OF8EKL"><img
-              src="https://imgur.com/I7KKXoq.jpeg"
-              alt="Introduction to Python"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-H4OF8EKL">
+              <img
+                src="https://imgur.com/I7KKXoq.jpeg"
+                alt="Python Intermediate"
+              />
             </a>
-            <span>Introduction to Python | Jan 2025 | <a id="cert"
-                                                         href="https://www.sololearn.com/certificates/CC-H4OF8EKL">Certificate</a></span>
+            <span>Introduction to Python | Jan 2025 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CC-H4OF8EKL"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-YJ1Y8S6C"><img
-              src="https://i.ibb.co/yQg6Cdk/coding-for-data.jpg"
-              alt="Coding for Data"
-            />
+            <a href="https://www.sololearn.com/en/certificates/CC-YJ1Y8S6C">
+              <img
+                src="https://i.ibb.co/yQg6Cdk/coding-for-data.jpg"
+                alt="Coding for Data"
+              />
             </a>
-            <span>Coding for Data | Dec 2024 | <a id="cert"
-                                                  href="https://www.sololearn.com/en/certificates/CC-YJ1Y8S6C">Certificate</a></span>
+            <span>Coding for Data | Dec 2024 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/en/certificates/CC-YJ1Y8S6C"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-5NVB5KDV"><img
-              src="https://i.imgur.com/EvogYB5.jpeg"
-              alt="SQL Intermediate"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-5NVB5KDV">
+              <img
+                src="https://i.imgur.com/EvogYB5.jpeg"
+                alt="SQL Intermediate"
+              />
             </a>
-            <span>SQL Intermediate | Nov 2024 | <a id="cert"
-                                                   href="https://www.sololearn.com/en/certificates/CC-5NVB5KDV">Certificate</a></span>
+            <span>SQL Intermediate | Nov 2024 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/en/certificates/CC-5NVB5KDV"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-FJIJVBP3"><img
-              src="https://i.imgur.com/iRdKaGX.png"
-              alt="Java Intermediate"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-FJIJVBP3">
+              <img
+                src="https://i.imgur.com/iRdKaGX.png"
+                alt="Java Intermediate"
+              />
             </a>
-            <span>Java Intermediate | Sept 2023 | <a id="cert"
-                                                     href="https://www.sololearn.com/certificates/CC-FJIJVBP3">Certificate</a></span>
+            <span>Java Intermediate | Sept 2023 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CC-FJIJVBP3"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-WXYQA2GW"><img
-              src="https://i.imgur.com/P8qgBC5.jpg"
-              alt="JavaScript"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-WXYQA2GW">
+              <img
+                src="https://i.imgur.com/P8qgBC5.jpg"
+                alt="JavaScript"
+              />
             </a>
-            <span>JavaScript | Aug 2023 | <a id="cert"
-                                             href="https://www.sololearn.com/certificates/CC-WXYQA2GW">Certificate</a></span>
+            <span>JavaScript | Aug 2023 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CC-WXYQA2GW"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CC-DVJRYRU8"><img
-              src="https://i.imgur.com/MbrP2z0.jpg"
-              alt="SQL"
-            />
+            <a href="https://www.sololearn.com/certificates/CC-DVJRYRU8">
+              <img
+                src="https://i.imgur.com/MbrP2z0.jpg"
+                alt="SQL"
+              />
             </a>
-            <span>SQL | Aug 2023 | <a id="cert"
-                                      href="https://www.sololearn.com/certificates/CC-DVJRYRU8">Certificate</a></span>
+            <span>SQL | Aug 2023 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CC-DVJRYRU8"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
           <li>
-            <a href="https://www.sololearn.com/certificates/CT-6JS4MSTM"><img
-              src="https://i.imgur.com/aZbeRzk.jpeg"
-              alt="Java"
-            />
+            <a href="https://www.sololearn.com/certificates/CT-6JS4MSTM">
+              <img
+                src="https://i.imgur.com/aZbeRzk.jpeg"
+                alt="Java"
+              />
             </a>
-            <span>Java | March 2023 | <a id="cert"
-                                         href="https://www.sololearn.com/certificates/CT-6JS4MSTM">Certificate</a></span>
+            <span>Java | March 2023 |
+              <a
+                id="cert"
+                href="https://www.sololearn.com/certificates/CT-6JS4MSTM"
+              >
+                Certificate
+              </a>
+            </span>
           </li>
         </ul>
       </div>
-      <ContactIcons/>
+      <ContactIcons />
     </article>
   </Main>
 );
