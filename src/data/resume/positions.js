3b85e95a8ecbe06befe7cdd3c6909e070754c564
Lyle Christine <cxz@gmx.com>
2025-08-20 18:24:14 +0100

    tidy up

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const positions = [
   {
     company: 'CGI (INTERNAL)',
@@ -7,9 +5,7 @@ const positions = [
     link: 'https://www.cgi.com/sites/default/files/2022-05/cgi_empowered_learning_-_pupil_monitoring_and_tracking.pdf',
     daterange: 'Oct 2024 - Present',
     points: [
-      <b>Java (JDK 23) · Microservices · MongoDB · Azure · PostGreSQL · Gradle · Angular ·
-        TypeScript
-      </b>,
+      'Java (JDK 23) · Microservices · MongoDB · Azure · PostGreSQL · Gradle · Angular · TypeScript',
     ],
   },
   {
@@ -18,15 +14,9 @@ const positions = [
     link: 'https://www.ruralpayments.org/',
     daterange: 'March 2024 - Oct 2024',
     points: [
-      <b>Java (JDK 11) · JSP · Red Hat Fuse · Git/GitLab · OpenShift · Oracle · SQL Developer ·
-        SoapUI
-      </b>,
+      'Java (JDK 11) · JSP · Red Hat Fuse · Git/GitLab · OpenShift · Oracle · SQL Developer · SoapUI',
     ],
-    description: 'I joined the Agricultural & Rural Economies (ARE) project with the'
-      + ' Scottish Government as a Java Developer. The ARE project is closely linked with the '
-      + 'Scottish Rural Payments and Services facility (https://www.ruralpayments.org/). The '
-      + 'maintenance and development of the Rural Payments Service is an ongoing relationship '
-      + 'between CGI developers and the Scottish Government.',
+    description: 'I joined the Agricultural & Rural Economies (ARE) project with the Scottish Government as a Java Developer. The ARE project is closely linked with the Scottish Rural Payments and Services facility (https://www.ruralpayments.org/). The maintenance and development of the Rural Payments Service is an ongoing relationship between CGI developers and the Scottish Government.',
   },
   {
     company: 'HMCTS (CGI CLIENT)',
@@ -34,13 +24,9 @@ const positions = [
     link: 'https://www.gov.uk/government/organisations/hm-courts-and-tribunals-service',
     daterange: 'May 2023 - October 2023',
     points: [
-      <b>Angular 16 ∙ Azure ∙ JavaScript/TypeScript ∙ HTML5 ∙ CSS3 ∙ Git/GitHub ∙ Agile
-      </b>,
+      'Angular 16 ∙ Azure ∙ JavaScript/TypeScript ∙ HTML5 ∙ CSS3 ∙ Git/GitHub ∙ Agile',
     ],
-    description: 'I gained an introduction to Front-End Angular Developer on a modernisation project '
-      + 'for HM Courts & Tribunals Service. This greenfield project focused on migrating and '
-      + 'upgrading the legacy Digital Audio Recording Transcription and Storage system (DARTS) onto '
-      + 'a Azure\'s Kubernetes Cloud Platform.',
+    description: 'I gained an introduction to Front-End Angular Developer on a modernisation project for HM Courts & Tribunals Service. This greenfield project focused on migrating and upgrading the legacy Digital Audio Recording Transcription and Storage system (DARTS) onto a Azure\'s Kubernetes Cloud Platform.',
   },
   {
     company: 'CGI',
@@ -48,13 +34,9 @@ const positions = [
     link: 'https://www.cgi.com/en',
     daterange: 'April 2023 - Present',
     points: [
-      <b>Software Development · Software Engineering · Back-End Web Development · Cloud Computing
-      </b>,
+      'Software Development · Software Engineering · Back-End Web Development · Cloud Computing',
     ],
-    description: 'CGI is an information technology, consulting and systems integration company with '
-      + 'a global presence. I\'m a Software Engineer within CGI\'s Scottish Business Unit. '
-      + 'This role is dynamic, demanding and requires a variety of current tech skills applied in '
-      + 'various business environments.',
+    description: 'CGI is an information technology, consulting and systems integration company with a global presence. I\'m a Software Engineer within CGI\'s Scottish Business Unit. This role is dynamic, demanding and requires a variety of current tech skills applied in various business environments.',
   },
   {
     company: 'FSB Tech',
@@ -62,13 +44,9 @@ const positions = [
     link: 'https://fsbtech.com/',
     daterange: 'Nov 2022 - Dec 2022',
     points: [
-      <b>Java (JDK 8/11) ∙ Maven ∙ Linux<br />
-        PostgreSQL ∙ MongoDB ∙ BitBucket<br />
-      </b>,
+      'Java (JDK 8/11) ∙ Maven ∙ Linux ∙ PostgreSQL ∙ MongoDB ∙ BitBucket',
     ],
-    description: 'FSB Tech was a gaming-technology company which created online betting experiences. '
-      + 'Unfortunately they had to close their Glasgow office in late 2022 which resulted in the '
-      + 'entire development team being made redundant.',
+    description: 'FSB Tech was a gaming-technology company which created online betting experiences. Unfortunately they had to close their Glasgow office in late 2022 which resulted in the entire development team being made redundant.',
   },
   {
     company: 'OLM Systems',
@@ -76,20 +54,9 @@ const positions = [
     link: 'https://www.olmsystems.com/',
     daterange: 'Aug 2021 - Oct 2022',
     points: [
-      <b>Java (JDK 8/11) ∙ Quarkus ∙ Kafka<br />
-        PostgreSQL ∙ Kubernetes ∙ Maven<br />
-        Google Cloud Platform ∙ Linux (Ubuntu)<br />
-      </b>,
+      'Java (JDK 8/11) ∙ Quarkus ∙ Kafka ∙ PostgreSQL ∙ Kubernetes ∙ Maven ∙ Google Cloud Platform ∙ Linux (Ubuntu)',
     ],
-    description: 'As part of the integration team, I developed bespoke data services which sat'
-      + ' within OLM\'s health & social care software products.'
-      + ' This role focused on Java/backend development in which Quarkus and Spring Boot'
-      + ' frameworks supported a variety of microservices and web apps. OLM\'s'
-      + ' product suite remains very popular with local authorities and other public sector'
-      + ' organisations, and the integration team was responsible for developing ad-hoc solutions'
-      + ' to support customer\'s case management and data needs.'
-      + ' My skills in Java Microservice Development, Cloud Native Application Design,'
-      + ' and Enterprise Systems Integration were highly strengthened during my time at OLM.',
+    description: 'As part of the integration team, I developed bespoke data services which sat within OLM\'s health & social care software products. This role focused on Java/backend development in which Quarkus and Spring Boot frameworks supported a variety of microservices and web apps. OLM\'s product suite remains very popular with local authorities and other public sector organisations, and the integration team was responsible for developing ad-hoc solutions to support customer\'s case management and data needs. My skills in Java Microservice Development, Cloud Native Application Design, and Enterprise Systems Integration were highly strengthened during my time at OLM.',
   },
   {
     company: 'Virgin Money',
@@ -97,17 +64,9 @@ const positions = [
     link: 'https://uk.virginmoney.com/',
     daterange: 'Oct 2018 - Jun 2021',
     points: [
-      <b>
-        OpenShift ∙ GitHub ∙ Docker ∙ Bash ∙ Python<br />
-        Java (JDK 8) ∙ Spring Boot ∙ Gradle ∙ Maven<br />
-        Jenkins ∙ Nexus Repo ∙ MongoDB ∙ SonarQube <br />
-      </b>,
+      'OpenShift ∙ GitHub ∙ Docker ∙ Bash ∙ Python ∙ Java (JDK 8) ∙ Spring Boot ∙ Gradle ∙ Maven ∙ Jenkins ∙ Nexus Repo ∙ MongoDB ∙ SonarQube',
     ],
-    description: 'In this DevOps role I developed, supported and maintained'
-      + ' Virgin Money\'s Operational Platform which was a complex containerisation estate'
-      + ' comprising of Spring Boot microservices and React web applications. This role advanced'
-      + ' my DevOps skill-set in Back End Development, Microservice Technology and Continuous'
-      + ' Integration/Delivery.',
+    description: 'In this DevOps role I developed, supported and maintained Virgin Money\'s Operational Platform which was a complex containerisation estate comprising of Spring Boot microservices and React web applications. This role advanced my DevOps skill-set in Back End Development, Microservice Technology and Continuous Integration/Delivery.',
   },
 ];
 
